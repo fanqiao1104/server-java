f30fc4856660675ba4b9b586c49c876d7694c838
lijinlong <lijinlong@kuaihuoyun.com>
2021-01-12 15:08:46 +0800

    remove contentORderMapper

--- schedule/src/main/java/com/sleep/server/task/FetchOrderTask.java
+++ schedule/src/main/java/com/sleep/server/task/FetchOrderTask.java
@@ -126,8 +126,8 @@ public class FetchOrderTask extends  IJobHandler {
                         item.setDiscountAmount(itemObject.getBigDecimal("discountAmount"));
                         item.setExpireTime(itemObject.getDate("expireTime"));
                         item.setSupplierProportion(itemObject.getInt("supplierProportion"));
-                        XxlJobLogger.log("%s %s %s %s %s %s",item.getOrderNo(),item.getUserId(),item.getPayTime(),item.getUserType(),item.getOrderMoney(),item.getActualMoney());
-                        contentOrderMapper.insert(item);
+                        XxlJobLogger.log(item.getOrderNo()+" "+item.getUserId()+" "+item.getPayTime()+" "+item.getUserType()+" "+item.getOrderMoney()+" "+item.getActualMoney());
+                       // contentOrderMapper.insert(item);
                     }
                     if(size>=ps){
                         pn=pn+1;
